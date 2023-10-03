f09bcb872583caeadd39a91a8f9aefc72ca22a77
MonkeyD-Cat <141637307+MonkeyD-Cat@users.noreply.github.com>
2023-10-03 20:23:32 +0300

    Done some basic stuff to add an item, ITEM TEXTURE (W.I.P.)

--- src/main/java/net/mosaab/fabricmod/FabricMod.java
+++ src/main/java/net/mosaab/fabricmod/FabricMod.java
@@ -3,6 +3,7 @@ package net.mosaab.fabricmod;
 import net.fabricmc.api.ModInitializer;
 
 import net.minecraft.item.Item;
+import net.mosaab.fabricmod.item.ModItems;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -13,6 +14,6 @@ public class FabricMod implements ModInitializer {
 	@Override
 	public void onInitialize() {
 
-		LOGGER.info("Hello wordle!");
+		ModItems.registerModItems();
 	}
 }
--- src/main/java/net/mosaab/fabricmod/item/ModItems.java
+++ src/main/java/net/mosaab/fabricmod/item/ModItems.java
@@ -1,4 +1,28 @@
 package net.mosaab.fabricmod.item;
 
+import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
+import net.fabricmc.fabric.api.itemgroup.v1.FabricItemGroupEntries;
+import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroups;
+import net.minecraft.registry.Registries;
+import net.minecraft.registry.Registry;
+import net.minecraft.util.Identifier;
+import net.mosaab.fabricmod.FabricMod;
+
 public class ModItems {
+    public static final Item UNIONITE = registerItem("unionite", new Item(new FabricItemSettings()));
+
+    private static void addItemsToIngredientItemGroup(FabricItemGroupEntries entries) {
+        entries.add(UNIONITE);
+    }
+
+    private static Item registerItem(String name, Item item){
+        return Registry.register(Registries.ITEM, new Identifier(FabricMod.MOD_ID, name), item);
+    }
+    public static void registerModItems(){
+        FabricMod.LOGGER.info("Registering Mod Items For " + FabricMod.MOD_ID);
+
+        ItemGroupEvents.modifyEntriesEvent(ItemGroups.INGREDIENTS).register(ModItems::addItemsToIngredientItemGroup);
+    }
 }
